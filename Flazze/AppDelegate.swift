2a4fe7382de394996965d0ab015dd1e507c3ea3c
Buka Cakrawala <kaka10garuda@gmail.com>
2016-10-16 00:06:27 -0700

    Enabling Login/signup

--- Flazze/AppDelegate.swift
+++ Flazze/AppDelegate.swift
@@ -20,13 +20,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FIRApp.configure()
         
        
-        FIRAuth.auth()?.signIn(withEmail: "kaka10garuda@gmail.com", password: "usa12345", completion: { (user, error) in
-            if error == nil {
-               print(user?.email)
-            } else {
-                print(error?.localizedDescription)
-            }
-        })
+//        FIRAuth.auth()?.signIn(withEmail: "kaka10garuda@gmail.com", password: "usa12345", completion: { (user, error) in
+//            if error == nil {
+//               print(user?.email)
+//            } else {
+//                print(error?.localizedDescription)
+//            }
+//        })
         
         return true
     }
--- Flazze/ViewController.swift
+++ Flazze/ViewController.swift
@@ -24,6 +24,19 @@ class ViewController: UITableViewController {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //to check wether the user need to create an account or just login with their account
+        FIRAuth.auth()?.addStateDidChangeListener({ (auth, user) in
+            if let user = user {
+                print("Welcome \(user.email)")
+                self.startObservingDB()
+            } else {
+                print("You need to sign up/login first!")
+            }
+        })
+    }
+    
     
     func startObservingDB() {
         dbRef.observe(.value, with: { (snapshot) in
@@ -42,6 +55,40 @@ class ViewController: UITableViewController {
         }
     }
     
+    @IBAction func loginOrSignup(_ sender: AnyObject) {
+        let userAlert = UIAlertController(title: "Login/Signup", message: "Enter email and password", preferredStyle: .alert)
+        userAlert.addTextField { (emailField) in
+            emailField.placeholder = "Email"
+        }
+        userAlert.addTextField { (passField) in
+            passField.isSecureTextEntry = true
+            passField.placeholder = "Password"
+        }
+        
+        userAlert.addAction(UIAlertAction(title: "Sign in", style: .default, handler: { (action) in
+            let emailTextField = userAlert.textFields?.first
+            let passwordTextField = userAlert.textFields?.first
+            
+            FIRAuth.auth()?.signIn(withEmail: (emailTextField?.text)!, password: (passwordTextField?.text)!, completion: { (user, error) in
+                if error != nil {
+                    print(error?.localizedDescription)
+                }
+            })
+        }))
+        
+        userAlert.addAction(UIAlertAction(title: "Sign up", style: .default, handler: { (action) in
+            let emailTextField = userAlert.textFields?.first
+            let passwordTextField = userAlert.textFields?.first
+            
+            FIRAuth.auth()?.createUser(withEmail: (emailTextField?.text)!, password: (passwordTextField?.text)!, completion: { (user, error) in
+                if error != nil {
+                    print(error?.localizedDescription)
+                }
+            })
+        }))
+        
+        self.present(userAlert, animated: true, completion: nil)
+    }
 
     @IBAction func addButton(_ sender: AnyObject) {
         let alert = UIAlertController(title: "New Post", message: "Enter you post here!", preferredStyle: .alert)
@@ -52,7 +99,7 @@ class ViewController: UITableViewController {
             //to grab the post that the user has already type in 
             if let postContent = alert.textFields?.first?.text {
                 // this is the post object that can be passed to Firebase Database
-                let post  = Post(content: postContent, addedByUser: "Kaka")
+                let post  = Post(content: postContent, addedByUser: "user")
                 
                 let postRef = self.dbRef.child(postContent.lowercased())
                 
